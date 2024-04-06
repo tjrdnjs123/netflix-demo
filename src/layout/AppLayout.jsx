aab2f8dd3960080cf4f9f6ae1cbb69ab86bda21a
Seokwon Choi <cswcoco03112@gmail.com>
2024-04-07 06:49:15 +0900

    first

---
+++ src/layout/AppLayout.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Outlet } from "react-router-dom";
+import { IoSearch } from "react-icons/io5"; 
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container fluid>
+          <Navbar.Brand href="#"> <img
+              src={"https://images.ctfassets.net/4cd45et68cgf/4nBnsuPq03diC5eHXnQYx/d48a4664cdc48b6065b0be2d0c7bc388/Netflix-Logo.jpg"} // 로고 이미지 소스
+              width="30"
+              height="30"
+              className="logo-img"
+              alt="React Bootstrap logo"
+            />Navbar scroll</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/movies">Movies</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-danger">
+                <IoSearch />
+              </Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </div>
+  );
+};
+
+export default AppLayout;
