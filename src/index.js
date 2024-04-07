a624fb2efbdb16b743a2be1f171ebd8c1497bd7b
Seokwon Choi <cswcoco03112@gmail.com>
2024-04-08 02:44:55 +0900

    second

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient , QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const queryClient = new QueryClient();
 root.render(
+  <QueryClientProvider client={queryClient}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  <ReactQueryDevtools initialIsOpen={false} buttonPosition="top-right"/>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Banner from "./components/Banner/Banner";
 
+//1 . 배너 => popular 영화를 들고와서 첫 번째로 보여주자
+//2 . popular movie
+//3 . top rated movie
+//4. upcoming movie
 const Homepage = () => {
   return(
   <div>
     Homepage
+    <Banner/>
   </div>)
 };
 
