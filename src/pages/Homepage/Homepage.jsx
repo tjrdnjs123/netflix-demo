94824f62bc17297d6b6cf3ced403870aa2874743
Seokwon Choi <cswcoco03112@gmail.com>
2024-04-09 03:50:59 +0900

    third

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "./components/Banner/Banner";
+import PopularMovieSlide from "./components/PopularMovieSlide/PopularMovieSlide";
 
 //1 . 배너 => popular 영화를 들고와서 첫 번째로 보여주자
 //2 . popular movie
@@ -8,8 +9,8 @@ import Banner from "./components/Banner/Banner";
 const Homepage = () => {
   return(
   <div>
-    Homepage
     <Banner/>
+    <PopularMovieSlide/>
   </div>)
 };
 
---
+++ src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
+import { Alert } from "bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import MovieCard from "../MovieCard/MovieCard";
+import "./PopularMovieSlide.style.css"
+
+const PopularMovieSlide = () => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+  const { data, isLoading, isError, error } = usePopularMoviesQuery();
+
+  if (isLoading) {
+    return <h1>Loading</h1>;
+  }
+  if (isError) {
+    return <Alert varient="danger">{error.message}</Alert>;
+  }
+  return (
+    <div>
+      <Carousel
+        responsive={responsive}
+        infinite={true}
+        containerClass="carousel-container"
+        itemClass="carousel-item-padding-40-px"
+        centerMode={true}
+      >
+        {data.results.map((movie, index) => (
+          <MovieCard movie={movie} key={index} />
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+export default PopularMovieSlide;
